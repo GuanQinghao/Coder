617e9472fe7b38b473bdaff73b972e0315680ddc
GuanQinghao <qinghaoguan@163.com>
2019-05-08 19:21:17 +0800

    修改代码

--- Coder/Templates.bundle/Objective-C/Model.m
+++ Coder/Templates.bundle/Objective-C/Model.m
@@ -38,58 +38,23 @@
 }
 
 #pragma mark -
-/**
- 增-
- 
- @param parameters 请求参数
- @param success 请求成功回调
- @param failure 请求失败回调
- */
-+ (void)qh_create<name>WithParameters:(id)parameters success:(void(^)(BOOL status, NSNumber *code, NSString *message))success failure:(void(^)(NSNumber *code, NSString *message, NSError *error))failure {
++ (void)qh_create<name>WithParameters:(id)parameters handler:(void (^)(BOOL, NSNumber * _Nonnull, NSString * _Nonnull, id _Nonnull))handler {
     
 }
 
-/**
- 查-
- 
- @param parameters 请求参数
- @param success 请求成功回调
- @param failure 请求失败回调
- */
-+ (void)qh_read<name>WithParameters:(id)parameters success:(void(^)(BOOL status, NSNumber *code, NSString *message, <prefix><name>Model *model))success failure:(void(^)(NSNumber *code, NSString *message, NSError *error))failure {
++ (void)qh_read<name>WithParameters:(id)parameters handler:(void(^)(BOOL status, NSNumber *code, NSString *message, id data))handler {
     
 }
 
-/**
- 改-
- 
- @param parameters 请求参数
- @param success 请求成功回调
- @param failure 请求失败回调
- */
-+ (void)qh_update<name>WithParameters:(id)parameters success:(void(^)(BOOL status, NSNumber *code, NSString *message))success failure:(void(^)(NSNumber *code, NSString *message, NSError *error))failure {
++ (void)qh_update<name>WithParameters:(id)parameters handler:(void(^)(BOOL status, NSNumber *code, NSString *message, id data))handler {
     
 }
 
-/**
- 删-
- 
- @param parameters 请求参数
- @param success 请求成功回调
- @param failure 请求失败回调
- */
-+ (void)qh_delete<name>WithParameters:(id)parameters success:(void(^)(BOOL status, NSNumber *code, NSString *message))success failure:(void(^)(NSNumber *code, NSString *message, NSError *error))failure {
++ (void)qh_delete<name>WithParameters:(id)parameters handler:(void(^)(BOOL status, NSNumber *code, NSString *message, id data))handler {
     
 }
 
-/**
- 列表-
- 
- @param parameters 请求参数
- @param success 请求成功回调
- @param failure 请求失败回调
- */
-+ (void)qh_fetch<name>sWithParameters:(id)parameters success:(void(^)(BOOL status, NSNumber *code, NSString *message, NSArray<<prefix><name>Model *> *models))success failure:(void(^)(NSNumber *code, NSString *message, NSError *error))failure {
++ (void)qh_fetch<name>sWithParameters:(id)parameters handler:(void(^)(BOOL status, NSNumber *code, NSString *message, id data))handler {
     
 }
 
